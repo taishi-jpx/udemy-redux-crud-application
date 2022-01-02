cde7c2486900081add4fd0b71e0aaa7785e97ba3
t-taishi <t-taishi@jpx.co.jp>
2022-01-03 01:54:15 +0900

    neko

--- src/App.js
+++ src/App.js
@@ -1,6 +1,38 @@
+import React from 'react';
 
-function App() {
-  return <div><h1>Hello World</h1></div>;
+const App = () =>{
+  return (
+    <div>
+      <Cat />
+      <Cat />
+      <Cat />
+    </div>
+  )
 }
+
+const Cat = () =>{
+  return <div>Meow!</div>
+}
+
+// class App extends Component {
+//   render(){
+//     return (
+//       <React.Fragment>
+//         <label htmlFor="bar">bar</label>
+//         <input type="text" onChange={() => {console.log("I'm clicked!")}} />
+//       </React.Fragment>
+//     );
+//   }
+// }
+
+// class App extends Component {
+//   render(){
+//     return React.createElement(
+//       "div",
+//       null,
+//       "Hello,world!!"
+//     )
+//   }
+// }
 
 export default App;
