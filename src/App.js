97ddf42ddeeac637e3d711d21ab982afcb5f80d9
t-taishi <t-taishi@jpx.co.jp>
2022-01-03 14:26:48 +0900

    learn state

--- src/App.js
+++ src/App.js
@@ -1,31 +1,63 @@
-import React from 'react';
-import PropTypes from 'prop-types'
+import React,{ Component } from 'react';
 
-const App = () =>{
-  const profiles = [
-    {name: "Taro", age: 10},
-    {name: "Hanako", age: 1 },
-    {name: "NoName", age: 3 }
-  ]
-  return (
-    <div>
-      {
-        profiles.map((profile, index) =>{
-          return <User name={profile.name} age={profile.age} key={index} />
-        })
-      }
-    </div>
-  )
+const App = () => <Counter></Counter>
+
+class Counter extends Component {
+  constructor(props) {
+    super(props)
+    console.log(this.state)
+    this.state = {count:0}
+  }
+
+  handlePlusButton = () => {
+    console.log("handlePlusButton")
+    //状態変更のお決まりメソッド
+    this.setState({count: this.state.count + 1})
+  }
+
+  handleMinusButton = () => {
+    //状態変更のお決まりメソッド
+    this.setState({count: this.state.count - 1})
+  }
+
+  render() {
+      return (
+      <React.Fragment>
+        <div>count:{this.state.count}</div>
+        <button onClick={this.handlePlusButton}>+1</button>
+        <button onClick={this.handleMinusButton}>-1</button>
+      </React.Fragment>
+    )
+  }
 }
 
-const User = (props) =>{
-  return <div>Hi, I am {props.name}, and {props.age} years old!</div>
-}
+// import PropTypes from 'prop-types'
 
-User.propTypes = {
-  name: PropTypes.string,
-  age: PropTypes.number.isRequired
-}
+// const App = () =>{
+//   const profiles = [
+//     {name: "Taro", age: 10},
+//     {name: "Hanako", age: 1 },
+//     {name: "NoName", age: 3 }
+//   ]
+//   return (
+//     <div>
+//       {
+//         profiles.map((profile, index) =>{
+//           return <User name={profile.name} age={profile.age} key={index} />
+//         })
+//       }
+//     </div>
+//   )
+// }
+
+// const User = (props) =>{
+//   return <div>Hi, I am {props.name}, and {props.age} years old!</div>
+// }
+
+// User.propTypes = {
+//   name: PropTypes.string,
+//   age: PropTypes.number.isRequired
+// }
 
 // // デフォルトを設定可能
 // User.defaultProps = {
