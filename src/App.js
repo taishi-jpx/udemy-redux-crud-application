8bd1e63ae2ed70fd16d62fbf0aebab7eb2795e91
t-taishi <t-taishi@jpx.co.jp>
2022-01-03 13:47:05 +0900

    learn props

--- src/App.js
+++ src/App.js
@@ -1,17 +1,29 @@
 import React from 'react';
 
 const App = () =>{
+  const profiles = [
+    {name: "Taro", age: 10},
+    {name: "Hanako", age: 5},
+    {name: "NoName"}
+  ]
   return (
     <div>
-      <Cat />
-      <Cat />
-      <Cat />
+      {
+        profiles.map((profile, index) =>{
+          return <User name={profile.name} age={profile.age} key={index} />
+        })
+      }
     </div>
   )
 }
 
-const Cat = () =>{
-  return <div>Meow!</div>
+const User = (props) =>{
+  return <div>Hi, I am {props.name}, and {props.age} years old!</div>
+}
+
+// デフォルトを設定可能
+User.defaultProps = {
+  age: 1,
 }
 
 // class App extends Component {
